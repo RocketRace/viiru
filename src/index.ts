1e26f7a5e511df076b2f6f29a523f4737a917999
RocketRace <oliviaspalmu@gmail.com>
2024-08-19 11:32:40 +0300

    clean up comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // here be any-casts
 
-// looking at the dependencies, we've got
+// looking at the dependencies, we have
 import fs from 'node:fs';
 // utilities
 import core from 'viiru_core';
@@ -22,14 +22,14 @@ import SB from 'scratch-blocks';
 // scratch-blocks contains an UI implementation for the scratch block 
 // editor and the VM listens to specifically its events, and it also
 // offers convenience methods to interact with the VM's blocks (which
-// is exactly what this project is, a block editor)
+// is exactly what this project is, a block editor).
 //
 // but those block events are a pain in the ass to construct manually -
 // like, writing-template-xml-strings-in-json kind of a pain -
-// so I'd much rather pull in the dependency
+// so I'd much rather pull in the dependency.
 //
 // problem: it's a whole-ass UI designed for the browser, and therefore
-// uses all sorts of browser-only globals just to initialize
+// uses all sorts of browser-only globals just to initialize.
 // 
 // so that's that I'm doing, dumping browser-only globals into the NodeJS
 // runtime. a huge dependency tree never hurt anybody
@@ -44,6 +44,8 @@ const workspace = (SB as any).inject('fakeDiv', {});
 // one interesting this about the workspace is that it's entirely
 // unsynchronized with the VM. its only purpose is to create
 // block events that I can then pass through to the VM.
+// this can theoretically cause issues with conflicting IDs...
+// but that's rare enough that I'll ignore it completely.
 
 // == editor functions ==
 const loadProject = async (path: string) => {
@@ -57,10 +59,11 @@ const saveProject = async (path: string) => {
 }
 
 // this one is a bit weird because the VM will only accept fully 
-// fledged block data structures (and I don't want to reimplement
+// fledged block JSON structures (and I don't want to reimplement
 // every single scratch block), but the templates are only implemented
 // by scratch-blocks in its own internal representation format.
-// so I manually call the VM's event listener, pretending to be scratch-blocks
+// so I manually call the VM's event listener, pretending to be scratch-blocks,
+// post-initializing the block with some extra data.
 const createBlock = (opcode: string, id?: string) => {
     const block = workspace.newBlock(opcode, id);
     const event = new (SB as any).Events.Create(block);
