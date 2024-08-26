427ba56af47f06ad56d14727d4dbda934e7198b7
RocketRace <oliviaspalmu@gmail.com>
2024-08-26 11:39:43 +0300

    Don't redraw every frame

--- viiru_core/src/lib.rs
+++ viiru_core/src/lib.rs
@@ -63,47 +63,30 @@ fn tui_main(mut cx: FunctionContext) -> JsResult<JsUndefined> {
         runtime.scroll_x -= (runtime.viewport.x_max - runtime.viewport.x_min) / 2;
         runtime.scroll_y -= (runtime.viewport.y_max - runtime.viewport.y_min) / 2;
 
-        // let (start, _) = state.create_block_template("event_whenflagclicked")?;
-
-        // let (iff, _) = state.create_block_template("control_if_else")?;
-        // state.attach_next(&iff, &start)?;
-
-        // let (cond, cond_children) = state.create_block_template("sensing_touchingcolor")?;
-        // state.attach_input(&cond, &iff, "CONDITION", false)?;
-        // state.set_field(&cond_children[0], "COLOUR", "#FF0000", None)?;
-
-        // let (motion, _) = state.create_block_template("motion_movesteps")?;
-        // state.attach_input(&motion, &iff, "SUBSTACK", false)?;
-
-        // let (op, op_children) = state.create_block_template("operator_add")?;
-        // state.attach_input(&op, &motion, "STEPS", false)?;
-        // state.set_strumber_field(&op_children[0], "12.3")?;
-
-        // let (hide, _) = state.create_block_template("looks_hide")?;
-        // state.attach_next(&hide, &motion)?;
-
-        // let (show, _) = state.create_block_template("looks_show")?;
-        // state.attach_next(&show, &hide)?;
-
+        let mut needs_refresh = true;
         loop {
             queue!(stdout(), Clear(ClearType::All))?;
-            // draw_block(&state, &start, cursor_x, cursor_y)?;
             draw_viewport_border(&runtime)?;
             draw_marker_dots(&runtime)?;
             draw_cursor_lines(&runtime)?;
-            let mut placement_cache = HashMap::new();
-            for top_id in &runtime.top_level {
-                // if top_id != &start {
-                draw_block(
-                    &runtime,
-                    top_id,
-                    runtime.blocks[top_id].x / 50,
-                    runtime.blocks[top_id].y / 50,
-                    &mut placement_cache,
-                )?;
-                // }
+            if needs_refresh {
+                // TODO: implement some form of culling
+                let mut placement_grid = HashMap::new();
+                for top_id in &runtime.top_level {
+                    // if top_id != &start {
+                    draw_block(
+                        &runtime,
+                        top_id,
+                        runtime.blocks[top_id].x / 50,
+                        runtime.blocks[top_id].y / 50,
+                        &mut placement_grid,
+                    )?;
+                    // }
+                }
+                runtime.placement_grid = placement_grid;
+
+                needs_refresh = false;
             }
-            runtime.placement_grid = placement_cache;
             draw_cursor(&runtime)?;
             let position = format!("{},{}", runtime.cursor_x, runtime.cursor_y);
 
@@ -131,12 +114,14 @@ fn tui_main(mut cx: FunctionContext) -> JsResult<JsUndefined> {
                                 {
                                     runtime.scroll_x -= 1;
                                 }
+                                needs_refresh = true;
                             }
                             KeyCode::Char('j') => {
                                 runtime.cursor_y += 1;
                                 if runtime.cursor_y - runtime.scroll_y == runtime.viewport.y_max {
                                     runtime.scroll_y += 1;
                                 }
+                                needs_refresh = true;
                             }
                             KeyCode::Char('k') => {
                                 runtime.cursor_y -= 1;
@@ -144,28 +129,34 @@ fn tui_main(mut cx: FunctionContext) -> JsResult<JsUndefined> {
                                 {
                                     runtime.scroll_y -= 1;
                                 }
+                                needs_refresh = true;
                             }
                             KeyCode::Char('l') => {
                                 runtime.cursor_x += 1;
                                 if runtime.cursor_x - runtime.scroll_x == runtime.viewport.x_max {
                                     runtime.scroll_x += 1;
                                 }
+                                needs_refresh = true;
                             }
                             KeyCode::Char('H') => {
                                 runtime.scroll_x -= 1;
                                 runtime.cursor_x -= 1;
+                                needs_refresh = true;
                             }
                             KeyCode::Char('J') => {
                                 runtime.scroll_y += 1;
                                 runtime.cursor_y += 1;
+                                needs_refresh = true;
                             }
                             KeyCode::Char('K') => {
                                 runtime.scroll_y -= 1;
                                 runtime.cursor_y -= 1;
+                                needs_refresh = true;
                             }
                             KeyCode::Char('L') => {
                                 runtime.scroll_x += 1;
                                 runtime.cursor_x += 1;
+                                needs_refresh = true;
                             }
                             KeyCode::Char(' ') => {
                                 // interaction!
@@ -175,6 +166,7 @@ fn tui_main(mut cx: FunctionContext) -> JsResult<JsUndefined> {
                                 {
                                     runtime.cursor_x += 1;
                                 }
+                                needs_refresh = true;
                             }
                             _ => (),
                         }
